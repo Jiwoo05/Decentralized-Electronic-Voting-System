bf5f009683e4aeec4d072beb03d9eb52c2245fc3
kimtaegeun1 <xosldi0501@naver.com>
2022-08-20 17:56:35 +0900

    Update 02. rest_api.py

--- lab_server/02. rest_api.py
+++ lab_server/02. rest_api.py
@@ -1,4 +1,4 @@
-from flask import  Flask, jsonify, request
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 @app.route('/api1', methods=['GET'])
@@ -12,3 +12,4 @@ def f2():
     return jsonify(data)
 
 app.run()
+
